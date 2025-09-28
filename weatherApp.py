43a4591fdcc73da1a76455cbb1fad5621fc03b2c
kushyme <peterserik1106@gmail.com>
2025-09-28 12:07:58 +0200

    current work progress

--- weatherApp.py
+++ weatherApp.py
@@ -19,10 +19,14 @@ except requests.RequestException as e:
 latValue = results[0]["lat"]
 lonValue = results[0]["lon"]
 
-
+data = []
 try:
-    response = session.get(f"https://api.openweathermap.org/data/2.5/weather?lat={latValue}&lon={lonValue}&appid={api_key}")
-    if response.status_code == 200:
-        print(response.json())
+    response2 = session.get(f"https://api.openweathermap.org/data/2.5/weather?lat={latValue}&lon={lonValue}&appid={api_key}")
+    if response2.status_code == 200:
+        data = response2.json()
 except requests.RequestException as e:
     print(e)
+
+print(data)
+# tempKelvin = data[2]["temp"] not working yet
+#print(tempKelvin)

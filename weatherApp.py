77f782692c6f85298debbc678f75629910f7cba7
kushyme <peterserik325@yahoo.de>
2025-10-22 18:12:35 +0200

    WIP refactor

--- weatherApp.py
+++ weatherApp.py
@@ -5,28 +5,53 @@ from dotenv import load_dotenv
 from terminaltables3 import AsciiTable
 import sys
 import * from fetchData
-load_dotenv()
-api_key = os.getenv('api_key')
-if not api_key:
-    print("no apikey found")
-    sys.exit(1)
 
-session = requests.session()
+
+
+def getApiKey():
+    load_dotenv()
+    api_key = os.getenv('api_key')
+    if not api_key:
+        raise RuntimeError("No API key found. Set API key in the .env file.")
+    return api_key
 
 def asciiMenu(data, cityName):
     table_data = [
-        ["Location", "Current temperature", "Min. temperature", "Max. temperature", "Humidity", "Description"],
-        [cityName, data["main"]["temp"], data["main"]["temp_min"], data["main"]["temp_max"], data["main"]["humidity"], data["weather"][0]["description"]]
+        [
+            "Location", 
+            "Current temperature",
+            "Min. temperature", 
+            "Max. temperature", 
+            "Humidity", 
+            "Description"
+         ],
+        [
+            cityName, 
+            data["main"]["temp"], 
+            data["main"]["temp_min"], 
+            data["main"]["temp_max"], 
+            data["main"]["humidity"], 
+            data["weather"][0]["description"]
+        ]
     ]
     table = AsciiTable(table_data)
     print(table.table)
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
+    if len(sys.argv) != 2: # might have to be a 3
         print("The right command is: weather <cityName> <countryCode>")
         sys.exit(1)
+
     cityName = sys.argv[1]
     countryCode = sys.argv[2]
-    latValue, lonValue = fetchLongitudeAndLatitude(cityName, countryCode)
-    data = fetchWeatherData(latValue, lonValue)
+
+    try:
+        api_key = getApiKey()
+        session = requests.session()
+        latitude, logitude = fetchCoordinates(cityName, countryCode) # TODO: add runtime error
+        data = fetchWeatherData(latitude, logitude) # TODO: add runtime error
+    except RuntimeError as e:
+        print(e)
+        sys.exit(1)
+
     asciiMenu(data, cityName)
